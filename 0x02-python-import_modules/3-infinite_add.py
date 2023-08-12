a0f7cbc23d9eee74b891f3a5b234547cd9dca63d
Ugwu F. B <fugwungene@gmail.com>
2023-08-12 21:37:08 +0000

    Remove def main()

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def main():
-    pass
-
 if __name__=="__main__":
     import sys
     nargs = len(sys.argv) - 1
@@ -9,4 +6,3 @@ if __name__=="__main__":
     for i in range(1, len(sys.argv)):
         sum1 += int(sys.argv[i])
     print("{:d}".format(sum1))
-    main()
