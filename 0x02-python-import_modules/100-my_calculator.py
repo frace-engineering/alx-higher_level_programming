a038166617e4d73c3fede69a2ec460ee4e405c77
Ugwu F. B <fugwungene@gmail.com>
2023-08-12 23:15:44 +0000

    Modify code
    Accommodate float variable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,23 @@ if __name__ == "__main__":
     from calculator_1 import add, mul, div, sub
     args = len(sys.argv) - 1
     result = 0
-    if (args != 4):
+    if (args != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if (sys.argv[2] == "+"):
         result = a + b
-        print("{:d} + {:d} = {:d}".format(a, b, result))
+        print("{} + {} = {}".format(a, b, result))
     elif (sys.argv[2] == "-"):
         result = a - b
-        print("{:d} + {:d} = {:d}".format(a, b, result))
+        print("{} + {} = {}".format(a, b, result))
     elif (sys.argv[2] == "*"):
         result = a * b
-        print("{:d} + {:d} = {:d}".format(a, b, result))
+        print("{} + {} = {}".format(a, b, result))
     elif (sys.argv[2] == "/"):
         result = a / b
-        print("{:d} + {:d} = {:d}".format(a, b, result))
+        print("{} + {} = {}".format(a, b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
