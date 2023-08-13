fc71d9f92cec0a31ff248090ffdda68f817de644
Ugwu F. B <fugwungene@gmail.com>
2023-08-13 21:11:46 +0000

    Use the imported modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,25 +2,25 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, mul, div, sub
-    args = len(sys.argv) - 1
+    args = len(sys.argv) 
     result = 0
-    if (args != 3):
+    if (args != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if (sys.argv[2] == "+"):
-        result = a + b
+        result = add(a, b) 
         print("{} + {} = {}".format(a, b, result))
     elif (sys.argv[2] == "-"):
-        result = a - b
-        print("{} + {} = {}".format(a, b, result))
+        result = sub(a, b)
+        print("{} - {} = {}".format(a, b, result))
     elif (sys.argv[2] == "*"):
-        result = a * b
-        print("{} + {} = {}".format(a, b, result))
+        result = mul(a, b) 
+        print("{} * {} = {}".format(a, b, result))
     elif (sys.argv[2] == "/"):
-        result = a / b
-        print("{} + {} = {}".format(a, b, result))
+        result = div(a, b)
+        print("{} / {} = {}".format(a, b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
