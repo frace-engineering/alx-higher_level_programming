de84920e562ef3f912ab56edcdaeb1ba71c1ff35
Ugwu F. B <fugwungene@gmail.com>
2023-08-13 21:14:32 +0000

    Comply to pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, mul, div, sub
-    args = len(sys.argv) 
+    args = len(sys.argv)
     result = 0
     if (args != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if (sys.argv[2] == "+"):
-        result = add(a, b) 
+        result = add(a, b)
         print("{} + {} = {}".format(a, b, result))
     elif (sys.argv[2] == "-"):
         result = sub(a, b)
         print("{} - {} = {}".format(a, b, result))
     elif (sys.argv[2] == "*"):
-        result = mul(a, b) 
+        result = mul(a, b)
         print("{} * {} = {}".format(a, b, result))
     elif (sys.argv[2] == "/"):
         result = div(a, b)
