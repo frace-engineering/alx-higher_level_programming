a5aed9b7dc94282e912029e7e3da8b41b1d7de98
Ugwu F. B <fugwungene@gmail.com>
2023-08-13 20:41:05 +0000

    Fix bug in code
    Remove the "- 1" in range

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     den = dir(hidden_4)
-    for i in range(len(den) - 1):
+    for i in range(len(den)):
         if den[i][:2] != "__":
             print("{:s}".format(den[i]))
