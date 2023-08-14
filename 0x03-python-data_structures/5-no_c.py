925605e4f43cafb1fa830ea19f45d4456a5904c0
Ugwu F. B <fugwungene@gmail.com>
2023-08-14 13:17:01 +0000

    Refactore code
    Use lower method instead of slicing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 def no_c(my_string):
     if my_string is None:
         return None
-    strlen = len(my_string)
-    str_idx = range(strlen - 2)
-    for i in str_idx:
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            my_string = my_string[:i] + my_string[i + 1:]
-    return my_string
+    new = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            new += char
+    return new
