c6381ea9ed1b3d93a4f6b2ea937e8317b5051bf4
Ugwu F. B <fugwungene@gmail.com>
2023-08-14 21:21:41 +0000

    Remove white space and use del keyword

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,7 @@ def delete_at(my_list=[], idx=0):
         return None
     list_len = len(my_list)
     if idx < 0 or idx >= list_len:
-        return my_list 
-    my_list = my_list[:idx] + my_list[idx + 1:]
-    return my_list
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
