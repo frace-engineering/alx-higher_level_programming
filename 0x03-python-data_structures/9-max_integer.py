cc01fcbd86231ec0e71b42210585bbe9c0e1dd9c
Ugwu F. B <fugwungene@gmail.com>
2023-08-16 00:25:48 +0000

    Refactor code
    Use sort method

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,5 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    for i in range(len(my_list)):
-        for j in range(1, len(my_list)):
-            if my_list[i] > my_list[j]:
-                return my_list[i]
-            else:
-                return my_list[j]
+    my_list.sort()
+    return my_list[-1]
