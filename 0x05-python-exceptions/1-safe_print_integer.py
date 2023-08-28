29381dcc04284421d0f6699188f0792e2ef37a9b
Ugwu F. B <fugwungene@gmail.com>
2023-08-28 23:14:17 +0000

    Refactor code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        if isinstance(value, int):
+    if isinstance(value, int):
+        try:
             print("{:d}".format(value))
             return (True)
-    except ValueError:
-        return (False)
+        except ValueError:
+            return (False)
