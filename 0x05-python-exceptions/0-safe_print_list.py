fb06d0815ddcbace99f9df75b4273563cfc374c5
Ugwu F. B <fugwungene@gmail.com>
2023-08-28 22:55:18 +0000

    Allow out of index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
                     print("{:d}".format(my_list[i]), end="")
                     num_of_elems += 1
             except IndexError:
-                break
+                continue
         print("")
     return (num_of_elems)
