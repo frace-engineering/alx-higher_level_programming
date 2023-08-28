b3336e2d8c20bb1cb4692c167c920f3df95d63fb
Ugwu F. B <fugwungene@gmail.com>
2023-08-28 18:09:19 +0000

    Fix requirement issues.
    Add ecexute mode to files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 num_of_elems += 1
-            except:
-                x is not int
+            except TypeError:
                 break
         print("")
     return (num_of_elems)
