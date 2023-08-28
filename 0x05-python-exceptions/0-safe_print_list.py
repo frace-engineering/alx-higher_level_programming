8c6aca18eb0c2a3a602069d438e982321e5d228c
Ugwu F. B <fugwungene@gmail.com>
2023-08-28 18:14:46 +0000

    Fix bug in code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 num_of_elems += 1
-            except TypeError:
-                break
+            except IndexError:
+                continue
         print("")
     return (num_of_elems)
