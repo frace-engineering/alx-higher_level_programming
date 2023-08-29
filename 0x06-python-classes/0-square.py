9a274e9f0be71a13ba52f25648493706205b0fac
Ugwu F. B <fugwungene@gmail.com>
2023-08-29 21:29:33 +0000

    Add prokect task files
    Add code lines to task 0 and task 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Create a Class Square
+
+"""
+
+class Square:
+    """ Define square """
+    def Square(self):
+        pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Creat a Class Square with private instance size
+
+"""
+
+class Square:
+    """ Define a square with size """
+    def __init__(self, size):
+        """ Initialize variables """
+        self.__size = size
