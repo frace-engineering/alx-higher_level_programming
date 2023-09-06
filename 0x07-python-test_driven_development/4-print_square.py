5ac1391406831d2b3e8888a6590f70be495609ff
Ugwu F. B <fugwungene@gmail.com>
2023-09-06 15:26:52 +0000

    Add code lines to task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that prints a square of '#' characters
+"""
+
+
+def print_square(size):
+    """Check if size is an integer else throw TypeError"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is None:
+        raise TypeError("size must be an integer")
+    if size == 0:
+        return None
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print()
