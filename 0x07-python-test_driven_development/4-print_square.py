de824c66b2ee2698ec554fce75d377e2eef63c6a
Ugwu F. B <fugwungene@gmail.com>
2023-09-06 15:32:58 +0000

    Fix bugs

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,7 +12,7 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if size is None:
         raise TypeError("size must be an integer")
-    if isinstance(size, float):
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     for i in range(size):
         for j in range(size):
