fa276a14748bc7629bbf687c1cd5cfb427998162
Ugwu F. B <fugwungene@gmail.com>
2023-09-06 09:34:37 +0000

    Add documentation to task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+The function "add_integer" adds two integers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    args:
+        a must be an integer or float
+        b must be an integer or float
+    raises:
+        TypeError if not correct type
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
@@ -8,4 +20,5 @@ def add_integer(a, b=98):
         raise OverflowError("a too large")
     if b + 1 == b:
         raise OverflowError("b too large")
+    """ Convert arg to int if they are float and return the sum """
     return int(a) + int(b)
