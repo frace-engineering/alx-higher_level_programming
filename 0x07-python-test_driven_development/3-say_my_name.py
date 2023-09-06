bff527c9ec3b7c647d8528608a5952f5db57e8a6
Ugwu F. B <fugwungene@gmail.com>
2023-09-06 14:48:35 +0000

    Fix bugs

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-A function that prints names 
+A function that prints names
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """Check if arguments are None, then throw TypeError"""
     if first_name is None and last_name is None:
         raise TypeError("say_my_name() missing 1 required"
                         " positional argument: 'first_name'")
+    """Chech the data type, if not string, throw TypeError"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
