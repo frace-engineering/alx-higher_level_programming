e53e9a7b7f6fb477262e5b6187281e255fc8c409
Ugwu F. B <fugwungene@gmail.com>
2023-09-06 14:43:42 +0000

    Add code lines to task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A function that prints names 
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if first_name is None and last_name is None:
+        raise TypeError("say_my_name() missing 1 required"
+                        " positional argument: 'first_name'")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("{:s} {:s}".format(first_name, last_name))
