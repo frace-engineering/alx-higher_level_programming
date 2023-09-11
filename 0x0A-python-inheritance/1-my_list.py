e6779cfb67ca97f51193211fc861baa0d55c8b9d
Ugwu F. B <fugwungene@gmail.com>
2023-09-11 17:21:13 +0000

    Modify code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,10 +4,8 @@ MyList is a subclass of list class
 """
 
 
-
 class MyList(list):
     """ Implement sorted list printing for the built-in list class """
     def print_sorted(self):
         """Print the list in ascending order"""
         print(sorted(self))
-
