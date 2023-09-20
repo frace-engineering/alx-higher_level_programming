d8f00b8e6e966d5697ac6406be8cfa04dd7c1db0
Ugwu F. B <fugwungene@gmail.com>
2023-09-20 15:57:15 +0000

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a Student class"""
+
+
+class Student:
+    """Instantiate the variables"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
