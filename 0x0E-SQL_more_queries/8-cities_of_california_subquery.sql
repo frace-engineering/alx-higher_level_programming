fd96f46116c4f4044b340c44d2438dcf4fa6d7fd
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 10:21:14 +0000

    Return cities.name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,8 +2,8 @@
 -- db name passed as argument
 USE hbtn_0d_usa;
 -- Seleect id and name columns of the table(cities) 
-SELECT id, name 
-FROM cities.name 
+SELECT id, cities.name 
+FROM cities
 -- Get the state_id from another select statement on the states table
 WHERE cities.state.id = (SELECT id FROM states WHERE name = 'California';)
 -- Order the result by id in ascending order
