72d729d54ec4de1a88544a93f94eedd65995c6f2
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 11:22:56 +0000

    More fixes

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- lists all cities of california
 -- db name passed as argument
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 SELECT id, name 
 FROM cities
-WHERE state_id IN (
+WHERE state_id = (
 	SELECT id 
 	FROM states 
 	WHERE name = 'California'
