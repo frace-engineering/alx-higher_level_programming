184df1dbfbb5e68b54b4781f112d8da354149512
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 11:14:33 +0000

    Fix yet another bog

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,11 @@
 -- lists all cities of california
 -- db name passed as argument
-USE hbtn_0d_usa;
--- Seleect id and name columns of the table(cities) 
+USE hbtn_0d_usa
 SELECT id, name 
 FROM cities
--- Get the state_id from another select statement on the states table
-WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
--- Order the result by id in ascending order
-ORDER BY id ASC;
+WHERE state_id IN (
+	SELECT id 
+	FROM states 
+	WHERE name = 'California'
+)
+ORDER BY id;
