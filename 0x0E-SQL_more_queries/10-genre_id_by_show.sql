086cdf95bc7ff50c4a4f7e34bccb4e2eaedc47f3
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 12:13:26 +0000

    Fix bug

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- db name will be passed as an argument
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id 
 FROM tv_shows 
 INNER JOIN tv_show_genres 
