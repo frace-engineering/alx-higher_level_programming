18e3475368229ba17eea327bb7034e04c4ed26a1
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 11:05:05 +0000

    Use correct syntax

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,9 +2,9 @@
 -- db name passed as argument
 USE hbtn_0d_usa;
 -- Seleect id and name columns of the table(cities) 
-SELECT cities.id AS id, name 
-FROM states.cities
+SELECT id, name 
+FROM cities
 -- Get the state_id from another select statement on the states table
-WHERE cities.id = (SELECT id FROM states WHERE name = 'California';)
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 -- Order the result by id in ascending order
 ORDER BY cities.id ASC;
