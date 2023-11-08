a7aba695b3e00b807a3ba10db4c929cc737eb71b
Ugwu Friday B <fugwungene@gmail.com>
2023-11-08 12:26:09 +0000

    Use database and order proparly.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- db name will be passed as an argument
+USE hbtn_0d_tvshows;
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id 
 FROM tv_shows 
 INNER JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+ON tv_show_genres.show_id = tv_shows.id 
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
