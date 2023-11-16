50b2498752d188d683e4a851224d5eaea29843b1
Ugwu Friday B <fugwungene@gmail.com>
2023-11-16 05:57:04 +0000

    Use concat operator

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 /* Print two arguments passed to the script, in the following format: <args2> “ is ” <args3>*/
-console.log(process.argv[2], ' is ',  process.argv[3]);
+console.log(process.argv[2] + ' is ' +  process.argv[3]);
