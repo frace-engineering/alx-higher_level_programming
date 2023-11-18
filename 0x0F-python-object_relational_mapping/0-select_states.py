60e8a4780ead1fb1233d029d419b05d021966709
Ugwu Friday B <fugwungene@gmail.com>
2023-11-18 23:29:34 +0000

    Fix bug in code
    Use pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,10 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cus = db.cursor()
-
     cus.execute("SELECT * FROM states ORDER BY id ASC")
-    
     [print(state) for state in cus.fetchall()]
-
     cus.close()
     db.close()
-
-
-
