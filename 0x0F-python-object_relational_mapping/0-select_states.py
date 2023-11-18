3582dfa6e2dec83c140ee4409b239bbaf42483bb
Ugwu Friday B <fugwungene@gmail.com>
2023-11-18 22:43:11 +0000

    Modify code
    Use localhost at port 3306

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cus = db.cursor()
 
