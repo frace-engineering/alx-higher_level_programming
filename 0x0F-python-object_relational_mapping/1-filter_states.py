a2fdd84afd713eeda2b6bcc3e2f371eb4e39f6a6
Ugwu Friday B <fugwungene@gmail.com>
2023-11-19 07:23:09 +0000

    Fix bug in code
    Use correct column name 'state'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cus = db.cursor()
-    cus.execute("SELECT * FROM states ORDER BY id ASC")
-    [print(state) for state in cus.fetchall() if state == ^[N.+]]
+    cus.execute("SELECT * FROM states WHERE state LIKE 'N%' ORDER BY id ASC")
+    [print(state) for state in cus.fetchall()]
     cus.close()
     db.close()
