f25aba77678705df8aedbee05b105cf825c4bd60
Ugwu Friday B <fugwungene@gmail.com>
2023-11-19 22:41:55 +0000

    Fix bug in code

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import Sequence, create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-
 """ python file that contains the class definition of a State
     and an instance Base = declarative_base()
 """
-DATABASE_URL = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sys import argv
+
 Base = declarative_base()
 
 
 class State(Base):
+    """ State class inherites from Base """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(238), nullable=False)
