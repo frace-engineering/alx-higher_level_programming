3eb62016ee479367fae8ed5695ddb324643dafaf
Ugwu Friday B <fugwungene@gmail.com>
2023-11-19 22:09:32 +0000

    Add comment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,10 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy import Sequence, create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
+
+""" python file that contains the class definition of a State
+    and an instance Base = declarative_base()
+"""
 DATABASE_URL = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
 Base = declarative_base()
 
