67513883f47e461dbd6cb764d887c664667720f3
Ugwu Friday B <fugwungene@gmail.com>
2023-11-19 23:32:57 +0000

    Add code lines to more task files
    Implement tasks 7, 8 and 9

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" List all State objects from the database hbtn_0e_6_usa
+    use sqlalchemy and should take 3 arguments
+"""
+
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+DB_URL = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+
+if __name__ == "__main__":
+    engine = create_engine(DB_URL, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 
+    lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+DB_URL = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+
+if __name__ == "__main__":
+    engine = create_engine(DB_URL, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        print(f"{state.id}: {state.name}")
