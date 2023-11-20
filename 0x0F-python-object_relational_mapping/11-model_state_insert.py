477aae34a635734054275db1d8a35d604e67f1b0
Ugwu Friday B <fugwungene@gmail.com>
2023-11-20 01:46:20 +0000

    Delete white space from the line

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state = State(name="Louisiana")
     session.add(state)
     session.commit()
