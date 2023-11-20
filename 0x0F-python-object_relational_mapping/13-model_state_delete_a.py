9dfb425ab40e6627ae32ca715d6967bbda55c5f2
Ugwu Friday B <fugwungene@gmail.com>
2023-11-20 02:09:45 +0000

    Fix bug in code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    for state in session.query(State):
         if 'a' in state.name:
             session.delete(state)
 
