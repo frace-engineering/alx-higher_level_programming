c6640cc47f8cff18e27690662027c57d53b36cad
Ugwu Friday B <fugwungene@gmail.com>
2023-12-01 15:22:55 +0000

    Include the output field

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,11 +14,13 @@ def find_peak(list_of_integers):
     elif size == 2:
         return max(list_of_integers)
 
-    mid = int(size / 2)
+    mid = size // 2
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+        high = find_peak(list_of_integers[mid:])
+        return high > peak ? high : peak 
+        low = find_peak(list_of_integers[:mid])
+        return low > peak ? high : peak 
+
+    
+
--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,11 @@
 # Python script that fetches a url using urllib
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    http = response.read()
+if __name__ == "__main__":
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t - type: {}'.format(type(html)))
+        print('\t - content: {}'.format(html))
+        print('\t - utf8 content: {}'.format(html.decode('utf-8')))
