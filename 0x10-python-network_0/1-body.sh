bc3bfd7026db40cff628c7dd52298d3410f7a747
Ugwu Friday B <fugwungene@gmail.com>
2023-12-01 07:23:32 +0000

    Modify code Use sudo command and -s flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes a url and send a GET  request to it
 #+ then displays the size of the body in bytes
-curl -sL "$1"
+sudo curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a DELETE request to the URL passed as argument and display the reponse
-sudo curl -X DELETE "$1"
+sudo curl -sX DELETE "$1"
