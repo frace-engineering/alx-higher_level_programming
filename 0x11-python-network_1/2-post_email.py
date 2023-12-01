6b46c6eca27b80fc2208e127a077ac8952ed901e
Ugwu Friday B <fugwungene@gmail.com>
2023-12-01 22:08:53 +0000

    Fix bug in code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,10 +11,9 @@ import sys
 if __name__ == "__main__":
     """Pass url and the email in the cli and print the response """
     url = sys.argv[1]
-    email = sys.argv[2].encode('utf-8')
-    param = urllib.parse.urlencode({'email': email})
-    full_url = f'{url}?{param}'
-    req = urllib.request.Request(url, email)
+    value = {'email': sys.argv[2]}
+    param = urllib.parse.urlencode(value).encode('ascii')
+    req = urllib.request.Request(url, param)
     with urllib.request.urlopen(req) as response:
         page_content = response.read().decode('utf-8')
-    print(f'Your email is: {page_content}')
+    print(f'{page_content}')
