96f8f34394c8e1e84de55c9d99683acb284f3b75
Ugwu Friday B <fugwungene@gmail.com>
2023-12-02 11:50:25 +0000

    Fix yet another bug

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,17 +2,17 @@
 """ Python script that takes in a url, sends a request
     to the url and displays the body of the request
     """
-
 import requests
 import sys
 
 
 if __namae__ == "__main__":
     """ Request and raise exception as error occures """
-    url = sys.argv[1]
     try:
+        url = sys.argv[1]
         res = requests.get(url)
         print(res.text)
+    """ Raise HTTPRrror and print status code if >= 400 """    
     except requests.exceptions.RequestException as er:
         if res.status_code >= 400:
             print(f'Error code: {res.status_code}')
