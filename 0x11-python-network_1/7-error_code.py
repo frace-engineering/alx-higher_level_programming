8ba9ed922c74d511f676c8f34f01752750c98a52
Ugwu Friday B <fugwungene@gmail.com>
2023-12-02 12:03:08 +0000

    Fix typo in name

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,13 +6,13 @@ import requests
 import sys
 
 
-if __namae__ == "__main__":
+if __name__ == "__main__":
     """ Request and raise exception as error occures """
     try:
         url = sys.argv[1]
         res = requests.get(url)
+        res.raise_for_status()
         print(res.text)
     """ Raise HTTPRrror and print status code if >= 400 """    
     except requests.exceptions.RequestException as er:
-        if res.status_code >= 400:
-            print(f'Error code: {res.status_code}')
+        print(f'Error code: {res.status_code}')
