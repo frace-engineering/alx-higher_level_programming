3ed1022be3beb3d3c537863312077a8931d788a1
Ugwu Friday B <fugwungene@gmail.com>
2023-12-03 22:13:07 +0000

    Fix code style error

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/** 
+/**
  * Square class that defines a square and inherits from
  * Rectangle of 4-rectangle.js
  */
@@ -7,8 +7,8 @@ const Rectangle = require('./4-rectangle');
 
 /* Use class notation and extends */
 class Square extends Rectangle {
-	constructor(size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
 module.exports = Square;
