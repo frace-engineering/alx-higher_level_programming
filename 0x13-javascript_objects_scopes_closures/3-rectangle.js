927cb3a8523c8f8429ad25573f79a6df47962ee2
Ugwu Friday B <fugwungene@gmail.com>
2023-12-03 18:12:06 +0000

    Add task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
+/**
+ * Create an empty class if width or height <= 0 
+ * print an instance of therectangle using
+ * character x
+ */
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       [this.width, this.height] = [w, h];
     }
   }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
+	print() {
+	for (let i = 0; i < this.height; i++) {
+		let row = '';
+		for (let j = 0; j < this.width; j++) {
+			row += 'X';
+		}
+		console.log(row);
+	}
+     }
 };
