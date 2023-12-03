b94ffa74f520ce740b2dd97eaeffbbdd45978e61
Ugwu Friday B <fugwungene@gmail.com>
2023-12-03 17:41:45 +0000

    Fix bug in code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,10 @@
 /* Create an empty class if width or height <= 0 */
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 || h > 0) {
+    if (w > 0 && h > 0) {
       [this.width, this.height] = [w, h];
     }
+	else {
+	}
   }
 };
