5fc687073cd2de03e1a1750b5f65e640073e4d79
Ugwu Friday B <fugwungene@gmail.com>
2023-12-04 01:47:10 +0000

    Fix bugs in code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+""" Python script that lists 10 most recent commits from a given
+    repository. Arguments passed are owner_name and repository_name
+    """
 import requests
 import sys
 
+
 def fetch_commits(owner, repo, number=10):
+    """ Using the github API, format the url and prob it to return the
+    response to the requests call. Retrieving 10 commits.
+    """
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     params = {'per_page': number}
     response = requests.get(url, params=params)
@@ -13,7 +20,9 @@ def fetch_commits(owner, repo, number=10):
         print(f"Error: {response.status_code}")
         return None
 
+
 def display_commits(commits):
+    """ This function difinition diplays the commits """
     if commits:
         for commit in commits:
             sha = commit['sha']
@@ -21,6 +30,7 @@ def display_commits(commits):
             print(f"{sha}: {author_name}")
     else:
         print("Failed to fetch commits.")
+
 
 if __name__ == "__main__":
     repository_name = sys.argv[1]
